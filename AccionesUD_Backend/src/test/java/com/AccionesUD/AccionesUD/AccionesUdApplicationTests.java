f2322dbb4b83213d802482911c68af577b5cef06
Johan Sebastian Marin Rojas <josmarinr@gmail.com>
2025-05-26 17:48:23 -0500

    Inicio de proyecto, repositorios Backend y Frontend

---
+++ AccionesUD_Backend/src/test/java/com/AccionesUD/AccionesUD/AccionesUdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AccionesUD.AccionesUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccionesUdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
