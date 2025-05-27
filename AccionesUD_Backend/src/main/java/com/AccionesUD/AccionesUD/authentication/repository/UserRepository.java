032881720a3d596497beaa56d0fd4a6415cb1d5d
Iván López GitHub <idlopezd@udistrital.edu.co>
2025-05-27 00:25:47 -0500

    Definición de entidades User y Role, y repositorio JPA.

--- AccionesUD_Backend/src/main/java/com/AccionesUD/AccionesUD/authentication/repository/UserRepository.java
+++ AccionesUD_Backend/src/main/java/com/AccionesUD/AccionesUD/authentication/repository/UserRepository.java
@@ -1,5 +1,14 @@
 package com.AccionesUD.AccionesUD.authentication.repository;
 
-public class UserRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.AccionesUD.AccionesUD.authentication.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsById(Double id);
 
 }
