0e56752c988c0ea16ad74d8457ed77c54d2f7c7c
Iván López GitHub <idlopezd@udistrital.edu.co>
2025-05-27 00:46:57 -0500

    Implementación de servicios de autenticación y correo electrónico para envío de notificaciones y códigos.

---
+++ AccionesUD_Backend/src/main/java/com/AccionesUD/AccionesUD/authentication/service/EmailService.java
@@ -0,0 +1,21 @@
+package com.AccionesUD.AccionesUD.authentication.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender mailSender;
+
+    public void sendOtpEmail(String to, String otp) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("Your OTP Code");
+        message.setText("Your one-time code is: " + otp);
+        mailSender.send(message);
+    }
+}
