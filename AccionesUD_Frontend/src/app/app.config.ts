1c2d08fab5e85e79ca699c8a155bf22d3ef76fe8
Iván López <ivan.lopez1307@gmail.com>
2025-06-09 22:24:05 -0500

    refactor(auth): ajustes responsive login + fix envío de token para recuperación

--- AccionesUD_Frontend/src/app/app.config.ts
+++ AccionesUD_Frontend/src/app/app.config.ts
@@ -1,14 +1,13 @@
 // app.config.ts
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http'; // ✅ IMPORTA ESTO
-
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(), 
+    provideHttpClient(),
   ],
 };
