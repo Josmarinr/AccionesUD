f9497e9fd18dd4e8ba5a979094f55733f3e8b7ba
Iván López <ivan.lopez1307@gmail.com>
2025-06-12 02:55:08 -0500

    Implementación de los métodos necesarios en el Frontend para que el Backend pueda enviar las notificaciones.

--- AccionesUD_Backend/src/main/java/com/AccionesUD/AccionesUD/domain/model/notification/Notification.java
+++ AccionesUD_Backend/src/main/java/com/AccionesUD/AccionesUD/domain/model/notification/Notification.java
@@ -17,8 +17,16 @@ public class Notification {
     private String recipient;
 
     private String type;
+
+    @Column(nullable = false)
+    private String title;
+
     private String message;
+    
     private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private boolean read = false; 
 
     public Notification() {
         this.createdAt = LocalDateTime.now();
--- AccionesUD_Frontend/src/app/app.routes.ts
+++ AccionesUD_Frontend/src/app/app.routes.ts
@@ -2,16 +2,18 @@ import { Routes } from '@angular/router';
 import { VistaRegistroComponent } from './vista-registro/vista-registro.component';
 import { CuerpoPrincipalComponent } from './cuerpo-principal/cuerpo-principal.component';
 import { MenuComponent } from './menu/menu.component';
+import { Menu2Component } from './menu2/menu2.component';
 import { MiPerfilComponent } from './mi-perfil/mi-perfil.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuard } from './guards/auth.guard';
 import { OrdenesPersonalizadasComponent } from './ordenes-personalizadas/ordenes-personalizadas.component';
 import { RecuperarComponent } from './recuperar-contrasena/recuperar-contrasena.component';
+import { NotificacionesComponent } from './notificaciones/notificaciones.component';
 
 export const routes: Routes = [
   { path: '', component: CuerpoPrincipalComponent },
   { path: 'registro', component: VistaRegistroComponent },
-  //{ path: '', component: MenuComponent },
+  { path: 'login', component: Menu2Component, canActivate: [AuthGuard] },
   {
     path: 'dashboard',
     component: DashboardComponent,
@@ -19,8 +21,12 @@ export const routes: Routes = [
   },
   { path: 'miperfil', component: MiPerfilComponent, canActivate: [AuthGuard] },
   { path: 'reset-password', component: RecuperarComponent },
+  {
+    path: 'notificaciones',
+    component: NotificacionesComponent,
+    canActivate: [AuthGuard],
+  },
 ];
-
 /*
 export const routes: Routes = [
   { path: '', component: CuerpoPrincipalComponent },
