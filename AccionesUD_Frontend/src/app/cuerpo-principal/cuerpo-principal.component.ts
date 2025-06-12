a0fa4e92de084809637701854198aced73118b47
SalazarMalagon <andressalazar062858@gmail.com>
2025-06-11 22:11:23 -0500

    SCRUM-69- agregar lógica de autenticación y menú condicional en el componente principal

--- AccionesUD_Frontend/src/app/cuerpo-principal/cuerpo-principal.component.ts
+++ AccionesUD_Frontend/src/app/cuerpo-principal/cuerpo-principal.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
 import { PiePaginaPrincipalComponent } from '../pie-pagina-principal/pie-pagina-principal.component';
 import { RelojesComponent } from "../relojes/relojes.component";
+import { Menu2Component } from '../menu2/menu2.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cuerpo-principal',
-  imports: [MenuComponent, PiePaginaPrincipalComponent, RelojesComponent],
+  imports: [MenuComponent, PiePaginaPrincipalComponent, RelojesComponent, Menu2Component, CommonModule],
   templateUrl: './cuerpo-principal.component.html',
   styleUrl: './cuerpo-principal.component.css',
   standalone: true
 })
 export class CuerpoPrincipalComponent {
+  get usuarioAutenticado(): boolean {
+    const token = localStorage.getItem('jwt');
+    if (!token) return false;
+
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    return Date.now() < payload.exp * 1000;
+  }
 
 }
